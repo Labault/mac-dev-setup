eaf571d54dd92983175408bd21cd6a6734aba810
Labault <thibault.lafaurie.dev@hotmail.com>
2026-06-16 09:56:13 +0200

    ✨ feat(zsh): add curated Powerlevel10k skeleton

---
+++ configs/zsh/.p10k.zsh
@@ -0,0 +1,39 @@
+# Curated Powerlevel10k configuration for mac-dev-setup.
+#
+# This file is intended to be copied or linked to ~/.p10k.zsh.
+
+'builtin' 'local' '-a' 'p10k_config_opts'
+[[ ! -o 'aliases'         ]] || p10k_config_opts+=('aliases')
+[[ ! -o 'sh_glob'         ]] || p10k_config_opts+=('sh_glob')
+[[ ! -o 'no_brace_expand' ]] || p10k_config_opts+=('no_brace_expand')
+'builtin' 'setopt' 'no_aliases' 'no_sh_glob' 'brace_expand'
+
+() {
+  emulate -L zsh -o extended_glob
+
+  unset -m '(POWERLEVEL9K_*|DEFAULT_USER)~POWERLEVEL9K_GITSTATUS_DIR'
+
+  [[ $ZSH_VERSION == (5.<1->*|<6->.*) ]] || return
+
+  typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
+    os_icon
+    dir
+    vcs
+  )
+
+  typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
+    status
+    command_execution_time
+    background_jobs
+    php_version
+    phpenv
+    context
+  )
+
+  (( ! $+functions[p10k] )) || p10k reload
+}
+
+typeset -g POWERLEVEL9K_CONFIG_FILE=${${(%):-%x}:a}
+
+(( ${#p10k_config_opts} )) && setopt ${p10k_config_opts[@]}
+'builtin' 'unset' 'p10k_config_opts'
